dbc9e58132d1cd30a4b5c9c03817ab765fbb8f49
Rashmi kumari <102369387+Rashmi6203@users.noreply.github.com>
2023-02-13 12:31:12 +0530

    Add files via upload

---
+++ right shift operation.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void showbits(unsigned char);
+
+int main()
+{
+	unsigned char num=225,i,k;
+	
+	printf("\nDecimal %d is same as binary",i);
+	showbits(i);
+	
+	for(i=0;i<=5;i++)
+	{
+		k = num>>i;
+		printf("\n%d right shift %d gives",num,i);
+		showbits(k);
+		
+	}
+	return 0;
+}
+void showbits( unsigned char n)
+{
+	int i;
+	unsigned char j,k,andmask;
+	
+	for(i=7;i>=0;i--)
+	{
+		j = i;
+		andmask = 1<< j;
+		
+		k=n & andmask;
+		k==0?printf("0"):printf("1");
+	}
+}
